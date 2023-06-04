b7731155cf8c573588fa437543fed2a3e997b4ed
yuto0602 <rinrin.abc1818@gmail.com>
2023-06-04 14:54:20 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
   resources :users, only: [:create, :new, :edit, :update]
+  resources :rooms, only: [:new, :create]
 end
